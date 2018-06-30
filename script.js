69379a9429ad058193a5d5fe77af006ad65690db
sinahyfrag <40439234+sinahyfrag@users.noreply.github.com>
2018-06-29 17:49:31 -0700

    Project

--- script.js
+++ script.js
@@ -1,20 +1,76 @@
 // Global Variables
 var p5Canvas;
 var myName;
+var ballX;
+var ballY;
+var ballRadius;
+var ballXVelocity;
+var ballGrowth;
 
 // initialize global variables in setup() function
 function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]");
+  myName.html("Sinahy");
+  ballX = width / 2;
+  ballY = height / 2;
+  ballRadius = 40;
+  ballXVelocity = 10;
+  ballGrowth = 10;
 }
 
 
 function draw() {
-  background(0, 255, 0);
+  drawBall();
+
 }
 
-// create an animation function below, then call it in draw()
-// see example.js if you need a place to start!
-// when you're ready, be sure to change index.html to link to this script instead of example.js
+function drawBall() {
+  if(ballX >= width - ballRadius || ballX <= 0 + ballRadius) {
+    ballXVelocity *= -1;
+  }
+  var circumference = getCircumference(); // local variable
+  if(circumference >= 600 || circumference <= 160) {
+    ballGrowth *= -1;
+  }
+  ballX += ballXVelocity;
+  var ballDiameter = ballRadius * 2; 
+  fill("white");
+  ellipse(ballX, ballY, ballDiameter, ballDiameter);
+}
+
+function getCircumference() {
+  return 2 * PI * ballRadius;
+}
+
+function drawSquares(num) {
+  while(num > 0) {
+    var randomX = random(-.5);
+    var randomY = random(-.5); 
+    var randomLength = random(20, 80); 
+    var randomRed = random(255); 
+    var randomGreen = random(255);
+    var randomBlue = random(255);
+    fill(randomRed, randomGreen, randomBlue);
+    rect(randomX, randomY, randomLength, randomLength);
+    num--;
+  }
+}
+function drawSquaresAlt(num) {
+  for(var i = 0; i < num; i++) {
+    var randomX = random(-.5); 
+    var randomY = random(-.5); 
+    var randomLength = random(20, 80); 
+    var randomRed = random(255);
+    var randomGreen = random(255);
+    var randomBlue = random(255);
+    fill(randomRed, randomGreen, randomBlue);
+    rect(randomX, randomY, randomLength, randomLength);
+  }
+    angular.module('myApp', [])
+.controller('MainCtrl', function($scope) {
+  $scope.boxClass = true;
+});
+    
+}
